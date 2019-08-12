caaf779863f9864c8570342099c651252dcbff2f
Shijing Liu <liushijing123@gmail.com>
2019-08-12 01:41:58 -0400

    delete some useful information

--- js/engine.js
+++ js/engine.js
@@ -24,10 +24,6 @@ var Engine = (function (global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    // canvas.width = 505;
-    // canvas.height = 606;
-    //doc.body.appendChild(canvas);
-
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
      */
