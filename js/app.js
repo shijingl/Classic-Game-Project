f15d22d2ec7c316a45a6b3b8f6d738b109757c18
Shijing Liu <liushijing123@gmail.com>
2020-01-18 13:24:48 -0500

    revise the first class functions

--- js/app.js
+++ js/app.js
@@ -4,14 +4,21 @@ let _createClass = function () {
     function defineProperties(target, props) { 
         for (let i = 0; i < props.length; i++) { 
             let descriptor = props[i]; 
-            descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; 
-            if ("value" in descriptor) descriptor.writable = true; 
+            descriptor.enumerable = descriptor.enumerable || false; 
+            descriptor.configurable = true; 
+            if ("value" in descriptor) {
+                descriptor.writable = true; 
+            }
             Object.defineProperty(target, descriptor.key, descriptor); 
         } 
     } 
     return function (Constructor, protoProps, staticProps) { 
-        if (protoProps) defineProperties(Constructor.prototype, protoProps); 
-        if (staticProps) defineProperties(Constructor, staticProps); 
+        if (protoProps) {
+            defineProperties(Constructor.prototype, protoProps); 
+        }
+        if (staticProps) {
+            defineProperties(Constructor, staticProps); 
+        }
         return Constructor; 
     }; 
 }();
